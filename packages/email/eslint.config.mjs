e18cfc994012f35fcc8e1c007b6a7a2e8b9e410d
Ganesh Adimalupu <ganeshjohn253@gmail.com>
2026-05-04 18:44:48 +0530

    fix(email): fix eslint config import name

--- packages/email/eslint.config.mjs
+++ packages/email/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from '@openpass/eslint-config/base'
+import { config as baseConfig } from '@openpass/eslint-config/base'
 
 /** @type {import("eslint").Linter.Config[]} */
-export default [...config]
+export default [...baseConfig]
